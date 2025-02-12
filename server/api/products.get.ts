c179ea62300381f5c57be66f70cf72e5ee2a96dd
leo-rp <leorock1988@hotmail.com>
2025-02-12 16:52:56 -0500

    + api cache by hand

--- server/api/products.get.ts
+++ server/api/products.get.ts
@@ -1,5 +1,18 @@
-export default defineEventHandler(async () => {
-	console.log('fetching products')
-  const products = await $fetch('https://fakestoreapi.com/products')    
-  return products
+import { reactive  } from "vue";
+
+let products = reactive([])
+
+export default defineEventHandler(async () => {	
+  console.warn('products => ', products)
+  if(products.length) {
+    console.log('cached')
+    return products
+  }
+    console.log('fetch')
+    products = await $fetch('https://fakestoreapi.com/products')    
+    return products
   })
+
+
+
+
